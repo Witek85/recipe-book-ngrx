5283cce2c697d4afb9bbec4df467ea436fc837d3
Witek85 <witold.szklarski@gmail.com>
2020-04-19 00:33:40 +0200

    root store

---
+++ src/app/auth/store/auth.reducer.ts
@@ -0,0 +1,66 @@
+// import { Ingredient } from '../../shared/ingredient.model';
+// import * as ShoppingListActions from './shopping-list.actions';
+import { User } from '../user.model';
+
+export interface State {
+  user: User;
+}
+
+const initialState: State = {
+    user: null
+};
+
+export function authReducer(
+  state: State = initialState,
+  action
+) {
+//   switch (action.type) {
+//     case ShoppingListActions.ADD_INGREDIENT:
+//       return {
+//         ...state,
+//         ingredients: [...state.ingredients, action.payload]
+//       };
+//     case ShoppingListActions.ADD_INGREDIENTS:
+//       return {
+//         ...state,
+//         ingredients: [...state.ingredients, ...action.payload]
+//       };
+//     case ShoppingListActions.UPDATE_INGREDIENT:
+//       const ingredient = state.ingredients[state.editedIngredientIndex];
+//       const updatedIngredient = {
+//         ...ingredient,
+//         ...action.payload
+//       };
+//       const updatedIngredients = [...state.ingredients];
+//       updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
+//       return {
+//         ...state,
+//         ingredients: updatedIngredients,
+//         editedIngredient: null,
+//         editedIngredientIndex: -1
+//       };
+//     case ShoppingListActions.DELETE_INGREDIENT:
+//       return {
+//         ...state,
+//         ingredients: state.ingredients.filter((i, index) => {
+//           return index !== state.editedIngredientIndex;
+//         }),
+//         editedIngredient: null,
+//         editedIngredientIndex: -1
+//       };
+//     case ShoppingListActions.START_EDIT:
+//       return {
+//         ...state,
+//         editedIngredient: { ...state.ingredients[action.payload] },
+//         editedIngredientIndex: action.payload
+//       };
+//     case ShoppingListActions.STOP_EDIT:
+//       return {
+//         ...state,
+//         editedIngredient: null,
+//         editedIngredientIndex: -1
+//       };
+//     default:
+      return state;
+//   }
+}
