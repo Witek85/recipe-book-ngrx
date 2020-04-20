b52d165a4fea62f5a729608f2541c3183b5119b9
Witek85 <witold.szklarski@gmail.com>
2020-04-21 00:20:04 +0200

    ngrx login effect

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -2,6 +2,7 @@ import { Action } from '@ngrx/store';
 
 export const LOGIN_START = '[Auth] LOGIN_START';
 export const LOGIN = '[Auth] LOGIN';
+export const LOGIN_FAIL = '[Auth] LOGIN_FAIL';
 export const LOGOUT = '[Auth] LOGOUT';
 
 export class Login implements Action {
@@ -27,4 +28,10 @@ export class LoginStart implements Action {
       }) {}
 }
 
-export type AuthActions = Login | Logout | LoginStart;
+export class LoginFail implements Action {
+  readonly type = LOGIN_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export type AuthActions = Login | Logout | LoginStart | LoginFail;
