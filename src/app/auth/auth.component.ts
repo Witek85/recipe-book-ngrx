61c15dfe7b5eba946eb8bc4d60a14ef2be1a5c48
Witek85 <witold.szklarski@gmail.com>
2020-04-22 00:07:03 +0200

    cleanup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,15 +7,14 @@ import {
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 
-import { AuthService, AuthResponseData } from './auth.service';
+import { AuthService } from './auth.service';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import * as FromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
-import { Actions } from '@ngrx/effects';
 
 @Component({
   selector: 'app-auth',
@@ -31,8 +30,6 @@ export class AuthComponent implements OnInit, OnDestroy {
   private storeSub: Subscription;
 
   constructor(
-    private authService: AuthService,
-    private router: Router,
     private componentFactoryResolver: ComponentFactoryResolver,
     private store: Store<FromApp.AppState>
   ) {}
@@ -57,9 +54,6 @@ export class AuthComponent implements OnInit, OnDestroy {
     }
     const email = form.value.email;
     const password = form.value.password;
-
-    // let authObs: Observable<AuthResponseData>;
-
     this.isLoading = true;
 
     if (this.isLoginMode) {
@@ -75,23 +69,6 @@ export class AuthComponent implements OnInit, OnDestroy {
       }))
       // authObs = this.authService.signup(email, password);
     }
-
-
-
-    // authObs.subscribe(
-    //   resData => {
-    //     console.log(resData);
-    //     this.isLoading = false;
-    //     this.router.navigate(['/recipes']);
-    //   },
-    //   errorMessage => {
-    //     console.log(errorMessage);
-    //     this.error = errorMessage;
-    //     this.showErrorAlert(errorMessage);
-    //     this.isLoading = false;
-    //   }
-    // );
-
     form.reset();
   }
 
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, tap, take } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 
 import { AuthService } from './auth.service';
@@ -37,11 +37,6 @@ export class AuthGuard implements CanActivate {
         }
         return this.router.createUrlTree(['/auth']);
       })
-      // tap(isAuth => {
-      //   if (!isAuth) {
-      //     this.router.navigate(['/auth']);
-      //   }
-      // })
     );
   }
 }
